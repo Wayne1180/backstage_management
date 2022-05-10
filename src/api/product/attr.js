3ec97806e2c74ef24e8472a60213da1719fc1f6f
Wayne1180 <xutenglongxutenglong@outlook.com>
2022-05-10 23:24:41 +0800

    属性的增删改操作完毕

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -9,3 +9,11 @@ export const reqCategory2List = (category1Id) => request({ url: `/admin/product/
 
 //获取三级分类数据接口
 export const reqCategory3List = (category2Id) => request({ url: `/admin/product/getCategory3/${category2Id}`, method: 'get' })
+
+//获取平台属性的接口
+export const reqAttrList = (category1Id, category2Id, category3Id) => request({ url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`, method: 'get' })
+
+//添加属性与属性值接口
+export const reqAddOrUpdateAttr = (data) => request({ url: '/admin/product/saveAttrInfo', method: 'post', data })
+
+//
